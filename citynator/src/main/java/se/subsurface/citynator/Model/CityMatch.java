2d845059e3ea7949c5f71f4add2ad9bb465d138b
aksel <willgurht@gmail.com>
2016-11-06 12:36:43 +0100

    new

---
+++ citynator/src/main/java/se/subsurface/citynator/Model/CityMatch.java
@@ -0,0 +1,9 @@
+package se.subsurface.citynator.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityMatch {
+    List<RoundResult> playerOneResults = new ArrayList<>();
+    List<RoundResult> playerTwoResults = new ArrayList<>();
+}
